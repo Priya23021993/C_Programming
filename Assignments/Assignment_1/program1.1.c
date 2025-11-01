cf39f3d07ef642403026e222987afa831364ea28
Priyanka Vikas Bhor <125297313+Priya23021993@users.noreply.github.com>
2025-11-01 05:56:04 -0600

    Improve comments and variable naming in program1.1.c
    Refactor comments and variable names for clarity.

--- Assignments/Assignment_1/program1.1.c
+++ Assignments/Assignment_1/program1.1.c
@@ -1,24 +1,49 @@
+/////////////////////////////////////////////////////////
+//
+//  Required header file
+//
+/////////////////////////////////////////////////////////
 #include<stdio.h>
 
-int Divide(int iNo1 , int iNo2)
+/////////////////////////////////////////////////////////
+//
+//  Function Name:  Divide
+//  Description:    It is use to perform division.
+//  Input:          integer
+//  Output:         integer
+//  Author:         Priyanka Vikas Bhor
+//  Date:           15/10/2025
+//
+/////////////////////////////////////////////////////////
+
+int Divide(   int iNo1 ,            //First Input
+               int iNo2             //Second input
+          ) 
 {
-    int iAns = 0;
+    int iAns = 0;                   //to stoar the result                    
 
     if(iNo2 > iNo1)
     {
-        return -1;
+        return -1;                
     }
-    iAns = iNo1 / iNo2;
+    iAns = iNo1 / iNo2;               //Business logic
     return iAns;
-}
+}    //End of Divide
+
+/////////////////////////////////////////////////////////
+//
+//  Entery point function for the Application
+//
+/////////////////////////////////////////////////////////
+
 int main()
 {
-    int iValue1 = 15, iValue2= 5;
-    int iRet = 0;
+    int iValue1 = 15, iValue2= 5;       //to accept inputs
+    int iRet = 0;                    //to stoare the result
 
-    iRet = Divide(iValue1,iValue2);
+    iRet = Divide(iValue1,iValue2);       //Function call
 
     printf("Division is :%d",iRet);
 
     return 0;
-}
+}    //End of main
