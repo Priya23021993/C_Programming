a640ab2d52c55e63a11e38c85da08d237b9b57f5
Priyanka Vikas Bhor <125297313+Priya23021993@users.noreply.github.com>
2026-01-04 23:23:39 +0530

    Create program1.c

---
+++ Assignments/Assignment_35/program1.c
@@ -0,0 +1,39 @@
+/* Write a program which accept string from user and count number
+   of capital Characters.
+
+   Input:  "Marvellous Muliti OS"
+
+   Output:  4
+
+*/
+#include<stdio.h>
+
+int CountCapital(char *str)
+{
+    int iCnt = 0;
+
+    while(*str != '\0')
+    {
+        if(*str >= 'A' && *str <= 'Z')
+        {
+            iCnt++;
+        }
+        str++;
+    }
+    return iCnt;
+}
+
+int main()
+{
+    char arr[20];
+    int iRet = 0;
+
+    printf("Enter String : \n");
+    scanf("%[^'\n']s",arr);
+
+    iRet = CountCapital(arr);
+
+    printf("%d",iRet);
+
+    return 0;
+}
