e01ca740ca4df1117416269e700ca58576ef541d
Priyanka Vikas Bhor <125297313+Priya23021993@users.noreply.github.com>
2026-01-04 23:26:00 +0530

    Create program1.c

---
+++ Assignments/Assignment_36/program1.c
@@ -0,0 +1,35 @@
+/* write a program which accept string from user and convert it into
+   lower Case.
+
+   Input : "Marvellous Multi OS"
+
+   Output : marvellous multi os
+
+*/
+
+#include<stdio.h>
+
+void strlwrx(char *str)
+{
+    while(*str != '\0')
+    {
+        if(*str >= 'A' && *str <= 'Z')
+        {
+            *str = *str + 32;
+        } 
+        str++;
+    }
+}
+int main()
+{
+    char arr[20];
+
+    printf("Enetr String :\n");
+    scanf ("%[^'\n]s",arr);
+
+    strlwrx(arr);
+
+    printf("Modified String is : %s\n",arr);
+
+    return 0;
+}
