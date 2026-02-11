70cdfb29ffc6b18e636ce1cd91f53f3d043d0b74
Priyanka Vikas Bhor <125297313+Priya23021993@users.noreply.github.com>
2026-02-11 09:23:09 +0530

    Implement singly linked list with even number replacement
    This program implements a singly linked list with functions to insert nodes, display the list, and replace even numbers with zero.

---
+++ Assignments/Assignment_48/program1.c
@@ -0,0 +1,82 @@
+/* Consider Singly Linear Linked list to solve below problem statements
+
+  Replace Even Numbers with 0
+  void ReplaceEven(PPNODE Head);
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct node
+{
+   int Data;
+   struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+void InsertFirst(PPNODE Head, int No)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+    newn -> Data = No;
+    newn -> next = NULL;
+
+    if(*Head == NULL)
+    {
+        *Head = newn;
+    }
+    else
+    {
+        newn -> next = *Head;
+        *Head = newn;
+    }
+}
+
+void Display(PNODE Head)
+{
+    while(Head != NULL)
+    {
+        printf("| %d | ->",Head -> Data);
+        Head = Head -> next;
+    }
+    printf("NULL\n");
+}
+
+void ReplaceEven(PPNODE Head)
+{
+    PNODE temp = *Head;
+
+    while(temp != NULL)
+    {
+        if(temp ->Data % 2 == 0)
+        {
+            temp->Data=0;
+        }
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    PNODE First = NULL;
+
+    InsertFirst(&First,11);
+    InsertFirst(&First,20);
+    InsertFirst(&First,33);
+    InsertFirst(&First,40);
+    InsertFirst(&First,55);
+
+    printf("Linked List is :\n");
+    Display(First);
+
+    ReplaceEven(&First);
+    printf("After Replace Even with 0 :\n");
+    Display(First);
+    
+    return 0;
+}
